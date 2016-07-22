eb77932328b82d51fee840fc483c12d51eb8091b
swb <swbsin@163.com>
2016-07-22 18:31:58 +0800

    Update setupPbft.sh

--- hyperledger/setupPbft.sh
+++ hyperledger/setupPbft.sh
@@ -7,9 +7,20 @@ then
    exit
 fi
 
-apt-get update
-apt-get install git curl -y
+
+apt-get install curl -y
 
 wget -qO- https://get.docker.com/ | sh 
 sudo service docker stop
-sudo docker daemon --api-cors-header="*" -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock
+nohup sudo docker daemon --api-cors-header="*" -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock&
+
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+docker pull yeasy/hyperledger:latest && docker tag yeasy/hyperledger:latest hyperledger/fabric-baseimage:latest
+docker pull yeasy/hyperledger-peer:latest
+docker pull yeasy/hyperledger-membersrvc:latest
+
+cd pbft
+docker-compose up
