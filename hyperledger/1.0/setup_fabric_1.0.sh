81915af46e22b11423f33788d55e5bd8d340750f
qiang0723 <wqiang0723@gmail.com>
2017-02-16 17:23:39 +0800

    Fix issue #15:modify format and layout

--- hyperledger/1.0/setup_fabric_1.0.sh
+++ hyperledger/1.0/setup_fabric_1.0.sh
@@ -19,15 +19,15 @@ curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-docker pull yeasy/hyperledger-fabric-base:latest \
-  && docker pull yeasy/hyperledger-fabric-peer:latest \
-  && docker pull yeasy/hyperledger-fabric-orderer:latest \
-  && docker pull yeasy/hyperledger-fabric-ca:latest \
+docker pull yeasy/hyperledger-fabric-base:0.8.1 \
+  && docker pull yeasy/hyperledger-fabric-peer:0.8.1 \
+  && docker pull yeasy/hyperledger-fabric-orderer:0.8.1 \
+  && docker pull yeasy/hyperledger-fabric-ca:0.8.1 \
   && docker pull yeasy/blockchain-explorer:latest \
-  && docker tag yeasy/hyperledger-fabric-peer hyperledger/fabric-peer \
-  && docker tag yeasy/hyperledger-fabric-orderer hyperledger/fabric-orderer \
-  && docker tag yeasy/hyperledger-fabric-ca hyperledger/fabric-ca \
-  && docker tag yeasy/hyperledger-fabric-base hyperledger/fabric-baseimage \
-  && docker tag yeasy/hyperledger-fabric-base hyperledger/fabric-ccenv:x86_64-1.0.0-snapshot-preview
+  && docker tag yeasy/hyperledger-fabric-peer:0.8.1 hyperledger/fabric-peer \
+  && docker tag yeasy/hyperledger-fabric-orderer:0.8.1 hyperledger/fabric-orderer \
+  && docker tag yeasy/hyperledger-fabric-ca:0.8.1 hyperledger/fabric-ca \
+  && docker tag yeasy/hyperledger-fabric-base:0.8.1 hyperledger/fabric-baseimage \
+  && docker tag yeasy/hyperledger-fabric-base:0.8.1 hyperledger/fabric-ccenv:x86_64-1.0.0-snapshot-preview
 
 docker-compose up
