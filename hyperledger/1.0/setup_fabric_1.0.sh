4b0d8ff15dada5a8c75f3a0858178be8679294e7
qiang0723 <wqiang0723@gmail.com>
2017-02-20 13:12:36 +0800

    modify the images tag to 0.8.3

--- hyperledger/1.0/setup_fabric_1.0.sh
+++ hyperledger/1.0/setup_fabric_1.0.sh
@@ -22,16 +22,16 @@ docker-compose --version
 ARCH=x86_64
 BASE_VERSION=1.0.0-preview
 PROJECT_VERSION=1.0.0-preview
-docker pull yeasy/hyperledger-fabric-base:0.8.2 \
-  && docker pull yeasy/hyperledger-fabric-peer:0.8.2 \
-  && docker pull yeasy/hyperledger-fabric-orderer:0.8.2 \
-  && docker pull yeasy/hyperledger-fabric-ca:0.8.2 \
+docker pull yeasy/hyperledger-fabric-base:0.8.3 \
+  && docker pull yeasy/hyperledger-fabric-peer:0.8.3 \
+  && docker pull yeasy/hyperledger-fabric-orderer:0.8.3 \
+  && docker pull yeasy/hyperledger-fabric-ca:0.8.3 \
   && docker pull yeasy/blockchain-explorer:latest \
-  && docker tag yeasy/hyperledger-fabric-peer:0.8.2 hyperledger/fabric-peer \
-  && docker tag yeasy/hyperledger-fabric-orderer:0.8.2 hyperledger/fabric-orderer \
-  && docker tag yeasy/hyperledger-fabric-ca:0.8.2 hyperledger/fabric-ca \
-  && docker tag yeasy/hyperledger-fabric-base:0.8.2 hyperledger/fabric-baseimage \
-  && docker tag yeasy/hyperledger-fabric-base:0.8.2 hyperledger/fabric-ccenv:$ARCH-$BASE_VERSION \
-  && docker tag yeasy/hyperledger-fabric-base:0.8.2 hyperledger/fabric-bareos:$ARCH-$BASE_VERSION
+  && docker tag yeasy/hyperledger-fabric-peer:0.8.3 hyperledger/fabric-peer \
+  && docker tag yeasy/hyperledger-fabric-orderer:0.8.3 hyperledger/fabric-orderer \
+  && docker tag yeasy/hyperledger-fabric-ca:0.8.3 hyperledger/fabric-ca \
+  && docker tag yeasy/hyperledger-fabric-base:0.8.3 hyperledger/fabric-baseimage \
+  && docker tag yeasy/hyperledger-fabric-base:0.8.3 hyperledger/fabric-ccenv:$ARCH-$BASE_VERSION \
+  && docker tag yeasy/hyperledger-fabric-base:0.8.3 hyperledger/fabric-baseos:$ARCH-$BASE_VERSION
 
 docker-compose up
