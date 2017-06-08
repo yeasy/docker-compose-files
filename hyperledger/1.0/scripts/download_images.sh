9fc9a11e120a1ef7effd7b1268a1c532548246c2
Baohua Yang <yangbaohua@gmail.com>
2017-06-08 11:44:31 +0800

    Fix image tags

--- hyperledger/1.0/scripts/download_images.sh
+++ hyperledger/1.0/scripts/download_images.sh
@@ -3,15 +3,17 @@
 source scripts/header.sh
 
 ARCH=x86_64
-BASE_VERSION=0.3.1
+BASEIMAGE_RELEASE=0.3.1
+BASE_VERSION=1.0.0
 PROJECT_VERSION=1.0.0-preview
-IMG_VERSION=0.9.4
 
+IMG_VERSION=0.9.4
 # For testing latest images
 IMG_VERSION=latest
 
 echo_b "Downloading images from DockerHub... need a while"
 
+# TODO: we may need some checking on pulling result?
 docker pull yeasy/hyperledger-fabric-base:$IMG_VERSION \
   && docker pull yeasy/hyperledger-fabric-peer:$IMG_VERSION \
   && docker pull yeasy/hyperledger-fabric-orderer:$IMG_VERSION \
@@ -26,6 +28,7 @@ docker tag yeasy/hyperledger-fabric-peer:$IMG_VERSION hyperledger/fabric-peer \
   && docker tag yeasy/hyperledger-fabric-orderer:$IMG_VERSION hyperledger/fabric-orderer \
   && docker tag yeasy/hyperledger-fabric-ca:$IMG_VERSION hyperledger/fabric-ca \
   && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-ccenv:$ARCH-$PROJECT_VERSION \
-  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-baseos:$ARCH-$BASE_VERSION
+  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-baseos:$ARCH-$BASE_VERSION \
+  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE
 
 echo_g "Done, now can startup the network using docker-compose..."
