4b4874571d5845f50bc4554bd1d6654492c7d0ca
Baohua Yang <yangbaohua@gmail.com>
2017-08-22 21:58:46 +0800

    Update image tag name

--- hyperledger/1.0/scripts/download_images.sh
+++ hyperledger/1.0/scripts/download_images.sh
@@ -17,31 +17,31 @@ BASE_VERSION=1.0.0
 PROJECT_VERSION=1.0.0
 
 # For testing 1.0.0 images
-IMG_VERSION=1.0.0
+IMG_TAG=1.0.0
 
 echo_b "Downloading images from DockerHub... need a while"
 
 # TODO: we may need some checking on pulling result?
-docker pull yeasy/hyperledger-fabric-base:$IMG_VERSION \
-  && docker pull yeasy/hyperledger-fabric-peer:$IMG_VERSION \
-  && docker pull yeasy/hyperledger-fabric-orderer:$IMG_VERSION \
-  && docker pull yeasy/hyperledger-fabric-ca:$IMG_VERSION \
-  && docker pull hyperledger/fabric-couchdb:$ARCH-$IMG_VERSION \
-  && docker pull hyperledger/fabric-kafka:$ARCH-$IMG_VERSION \
-  && docker pull hyperledger/fabric-zookeeper:$ARCH-$IMG_VERSION
+docker pull yeasy/hyperledger-fabric-base:$IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-peer:$IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-orderer:$IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-ca:$IMG_TAG \
+  && docker pull hyperledger/fabric-couchdb:$ARCH-$IMG_TAG \
+  && docker pull hyperledger/fabric-kafka:$ARCH-$IMG_TAG \
+  && docker pull hyperledger/fabric-zookeeper:$ARCH-$IMG_TAG
 
 # Only useful for debugging
 # docker pull yeasy/hyperledger-fabric
 
 echo_b "Rename images with official tags..."
-docker tag yeasy/hyperledger-fabric-peer:$IMG_VERSION hyperledger/fabric-peer \
-  && docker tag yeasy/hyperledger-fabric-peer:$IMG_VERSION hyperledger/fabric-tools \
-  && docker tag yeasy/hyperledger-fabric-orderer:$IMG_VERSION hyperledger/fabric-orderer \
-  && docker tag yeasy/hyperledger-fabric-ca:$IMG_VERSION hyperledger/fabric-ca \
-  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-ccenv:$ARCH-$PROJECT_VERSION \
-  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE \
-  && docker tag yeasy/hyperledger-fabric-base:$IMG_VERSION hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE \
-  && docker tag hyperledger/fabric-zookeeper:$ARCH-$IMG_VERSION hyperledger/fabric-zookeeper \
-  && docker tag hyperledger/fabric-kafka:$ARCH-$IMG_VERSION hyperledger/fabric-kafka
+docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-peer \
+  && docker tag yeasy/hyperledger-fabric-peer:$IMG_TAG hyperledger/fabric-tools \
+  && docker tag yeasy/hyperledger-fabric-orderer:$IMG_TAG hyperledger/fabric-orderer \
+  && docker tag yeasy/hyperledger-fabric-ca:$IMG_TAG hyperledger/fabric-ca \
+  && docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-ccenv:$ARCH-$PROJECT_VERSION \
+  && docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE \
+  && docker tag yeasy/hyperledger-fabric-base:$IMG_TAG hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE \
+  && docker tag hyperledger/fabric-zookeeper:$ARCH-$IMG_TAG hyperledger/fabric-zookeeper \
+  && docker tag hyperledger/fabric-kafka:$ARCH-$IMG_TAG hyperledger/fabric-kafka
 
 echo_g "Done, now can startup the network using docker-compose..."
