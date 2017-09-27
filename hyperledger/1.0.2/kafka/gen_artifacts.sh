1c874a84dc6690b702cb9c9e722d66c7037464c9
Baohua Yang <yangbaohua@gmail.com>
2017-09-27 14:52:27 +0800

    Enable kafka mode

--- hyperledger/1.0.2/kafka/gen_artifacts.sh
+++ hyperledger/1.0.2/kafka/gen_artifacts.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-GEN_IMG=yeasy/hyperledger-fabric:latest
+GEN_IMG=yeasy/hyperledger-fabric:1.0.2
 GEN_CONTAINER=generator
 CFG_DIR=/etc/hyperledger/fabric
 TMP_DIR=/tmp
 ARTIFACTS_DIR=$TMP_DIR/channel-artifacts
 CHANNEL_NAME=businesschannel
+
+echo "Clean potential existing container $GEN_CONTAINER"
+[ "$(docker ps -a | grep $GEN_CONTAINER)" ] && docker rm -f $GEN_CONTAINER
+
+echo "Remove existing artifacts"
+rm -rf crypto-config channel-artifacts
 
 echo "Starting container $GEN_CONTAINER in background"
 docker run \
@@ -24,6 +30,7 @@ docker cp ./configtx.yaml $GEN_CONTAINER:$CFG_DIR
 echo "Generating crypto-config and export"
 docker exec -it $GEN_CONTAINER \
 	cryptogen generate --config=$CFG_DIR/crypto-config.yaml --output $TMP_DIR/crypto-config
+echo "Export crypto-config to local"
 docker cp $GEN_CONTAINER:$TMP_DIR/crypto-config ./
 
 echo "Copy crypto-config to the config path"
@@ -32,7 +39,7 @@ docker exec -it $GEN_CONTAINER \
 
 echo "Generating orderer genesis block file"
 docker exec -it $GEN_CONTAINER \
-	configtxgen -profile TwoOrgsOrdererGenesis -outputBlock $ARTIFACTS_DIR/orderer_genesis.block
+	configtxgen -profile TwoOrgsOrdererGenesis -outputBlock $ARTIFACTS_DIR/orderer.genesis.block
 
 echo "Create the new application channel tx"
 docker exec -it $GEN_CONTAINER \
@@ -44,7 +51,7 @@ docker exec -it $GEN_CONTAINER \
 docker exec -it $GEN_CONTAINER \
 	configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate $ARTIFACTS_DIR/Org2MSPanchors.tx -channelID ${CHANNEL_NAME} -asOrg Org2MSP
 
-echo "Export $ARTIFACTS_DIR out"
+echo "Export $ARTIFACTS_DIR to local"
 docker cp $GEN_CONTAINER:$ARTIFACTS_DIR ./
 
 echo "Remove the container $GEN_CONTAINER"
