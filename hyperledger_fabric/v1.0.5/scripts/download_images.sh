21968059e2a6cb879318604ac704dab41693a862
Baohua Yang <yangbaohua@gmail.com>
2018-01-30 16:29:57 +0800

    Move crypto-config to upper level

--- hyperledger_fabric/v1.0.5/scripts/download_images.sh
+++ hyperledger_fabric/v1.0.5/scripts/download_images.sh
@@ -1,37 +1,40 @@
 #!/usr/bin/env bash
 
 ARCH=x86_64
-BASEIMAGE_RELEASE=0.3.2
-PROJECT_VERSION=1.0.2
 
-# For testing 1.0.0 images
-IMG_TAG=1.0.5
+# for the base images, including baseimage, baseos, couchdb, kafka, zookeeper
+BASE_IMG_TAG=0.3.2
+
+# For fabric images, including peer, orderer, ca
+FABRIC_IMG_TAG=1.0.2
 
 echo "Downloading images from DockerHub... need a while"
 
+echo "===Pulling base images from yeasy repo... with tag = ${BASE_IMG_TAG}"
+docker pull hyperledger/fabric-baseimage:$ARCH-$BASE_IMG_TAG
+docker pull hyperledger/fabric-baseos:$ARCH-$BASE_IMG_TAG
+docker pull hyperledger/fabric-couchdb:$ARCH-$BASE_IMG_TAG
+docker pull hyperledger/fabric-kafka:$ARCH-$BASE_IMG_TAG
+docker pull hyperledger/fabric-zookeeper:$ARCH-$BASE_IMG_TAG
+
 # TODO: we may need some checking on pulling result?
-docker pull yeasy/hyperledger-fabric-base:$IMG_TAG \
-  && docker pull yeasy/hyperledger-fabric-peer:$IMG_TAG \
-  && docker pull yeasy/hyperledger-fabric-orderer:$IMG_TAG \
-  && docker pull yeasy/hyperledger-fabric-ca:$IMG_TAG \
-  && docker pull hyperledger/fabric-couchdb:$ARCH-$IMG_TAG \
-  && docker pull hyperledger/fabric-kafka:$ARCH-$IMG_TAG \
-  && docker pull hyperledger/fabric-zookeeper:$ARCH-$IMG_TAG
+echo "===Pulling fabric images from yeasy repo... with tag = ${FABRIC_IMG_TAG}"
+docker pull yeasy/hyperledger-fabric-base:$FABRIC_IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-peer:$FABRIC_IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-orderer:$FABRIC_IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric:$FABRIC_IMG_TAG \
+  && docker pull yeasy/hyperledger-fabric-ca:$FABRIC_IMG_TAG \
+	&& docker pull docker pull yeasy/blockchain-explorer:0.1.0-preview  # TODO: wait for official images
 
 # Only useful for debugging
 # docker pull yeasy/hyperledger-fabric
 
-echo "===Pulling fabric images from official repo... with tag = ${IMG_TAG}"
-docker pull hyperledger/fabric-peer:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-tools:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-orderer:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-ca:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-ccenv:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-baseimage:$ARCH-$BASEIMAGE_RELEASE
-docker pull hyperledger/fabric-baseos:$ARCH-$BASEIMAGE_RELEASE
-docker pull hyperledger/fabric-couchdb:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-kafka:$ARCH-$IMG_TAG
-docker pull hyperledger/fabric-zookeeper:$ARCH-$IMG_TAG
+echo "===Pulling fabric images from official repo... with tag = ${FABRIC_IMG_TAG}"
+docker pull hyperledger/fabric-peer:$ARCH-$FABRIC_IMG_TAG
+docker pull hyperledger/fabric-tools:$ARCH-$FABRIC_IMG_TAG
+docker pull hyperledger/fabric-orderer:$ARCH-$FABRIC_IMG_TAG
+docker pull hyperledger/fabric-ca:$ARCH-$FABRIC_IMG_TAG
+docker pull hyperledger/fabric-ccenv:$ARCH-$FABRIC_IMG_TAG
 
 echo "Done, now can startup the network using docker-compose..."
 
