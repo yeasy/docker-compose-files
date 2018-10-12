0115e8175c259527ab0f4b38a7abac408e35b358
Baohua Yang <yangbaohua@gmail.com>
2018-10-12 21:31:06 +0800

    Generated credential's cn has no suffix

--- hyperledger_fabric/latest/ca-test/scripts/ca_server_start.sh
+++ hyperledger_fabric/latest/ca-test/scripts/ca_server_start.sh
@@ -4,6 +4,11 @@ source env.sh
 
 #TODO: check param number is 2
 
+if [ "$#" -ne 2 ]; then
+	echo "Illegal number of parameters"
+	exit 1
+fi
+
 ORG_NAME=$1  # org1.example.com
 TYPE=$2  # ca | tlsca
 
@@ -12,43 +17,40 @@ CA_PATH=${ORG_PATH}/${TYPE}  # e.g., /etc/hyperledger/fabric-ca-server/ca
 
 echo $(hostname)
 
-[ -d ${CA_PATH} ] || mkdir -p ${CA_PATH}
+# do not recreate the credentials if existed
+if [ ! -d ${CA_PATH} ]; then
+	mkdir -p ${CA_PATH}
+	cd ${CA_PATH}
+	echo "Generate the credentials for ${TYPE}.${ORG_NAME}"
 
-cd ${CA_PATH}
+	# generate fabric-ca-server-config.yaml
+	#fabric-ca-server init \
+	#	-H ${CA_PATH} \
+	#	-b ${DEFAULT_USER}:${DEFAULT_PASS}
+	#rm -rf msp/* ca-cert.pem  # these credentials are wrong
 
-echo $PWD # /etc/hyperledger/fabric-ca-server/ca
+	echo "${CA_SERVER_DEFAULT_CONFIG}" >> fabric-ca-server-config.yaml
 
-echo "Generate the credentials for ${TYPE}.${ORG_NAME}"
-#fabric-ca-server init --csr.cn=${ORG_NAME} -b admin:pass
-#mv ca-cert.pem ${ORG_NAME}-cert.pem
-#mv msp/keystore/*_sk ${ORG_NAME}_sk
+	# Update config
+	echo "Update fabric-ca-server-config.yaml"
+	yq w -i fabric-ca-server-config.yaml ca.name "${TYPE}.${ORG_NAME}"
+	yq w -i fabric-ca-server-config.yaml ca.certfile "${TYPE}.${ORG_NAME}-cert.pem"
+	yq w -i fabric-ca-server-config.yaml ca.keyfile "${TYPE}.${ORG_NAME}_sk"
 
-# generate fabric-ca-server-config.yaml
-fabric-ca-server init \
-	-H ${CA_PATH} \
-	-b ${DEFAULT_USER}:${DEFAULT_PASS}
+	yq w -i fabric-ca-server-config.yaml csr.cn "${TYPE}.${ORG_NAME}"
+	yq w -i fabric-ca-server-config.yaml csr.names[0].O "${ORG_NAME}"
+	yq w -i fabric-ca-server-config.yaml csr.names[0].OU "${TYPE}"
 
-rm -rf msp/* ca-cert.pem
+	yq w -i fabric-ca-server-config.yaml tls.enabled false
+	#yq w -i fabric-ca-server-config.yaml tls.certfile "${ORG_PATH}/tlsca/tlsca.${ORG_NAME}-cert.pem"
+	#yq w -i fabric-ca-server-config.yaml tls.keyfile "${ORG_PATH}/tlsca/tlsca.${ORG_NAME}_sk"
 
-# Update config
-echo "Update fabric-ca-server-config.yaml"
-yq w -i fabric-ca-server-config.yaml ca.name "${TYPE}.${ORG_NAME}"
-yq w -i fabric-ca-server-config.yaml ca.certfile "${TYPE}.${ORG_NAME}-cert.pem"
-yq w -i fabric-ca-server-config.yaml ca.keyfile "${TYPE}.${ORG_NAME}_sk"
+	# Generate new certs based on updated config
+	echo "Generate certificates for ${TYPE}.${ORG_NAME} under ${CA_PATH}"
+	fabric-ca-server init -H ${CA_PATH}
 
-yq w -i fabric-ca-server-config.yaml csr.cn "${TYPE}.${ORG_NAME}"
-yq w -i fabric-ca-server-config.yaml csr.names[0].O "${ORG_NAME}"
-yq w -i fabric-ca-server-config.yaml csr.names[0].OU "${TYPE}"
-
-yq w -i fabric-ca-server-config.yaml tls.enabled false
-#yq w -i fabric-ca-server-config.yaml tls.certfile "${ORG_PATH}/tlsca/tlsca.${ORG_NAME}-cert.pem"
-#yq w -i fabric-ca-server-config.yaml tls.keyfile "${ORG_PATH}/tlsca/tlsca.${ORG_NAME}_sk"
-
-# Generate new certs based on updated config
-echo "Generate certificates based on config"
-fabric-ca-server init -H ${CA_PATH}
-
-cp msp/keystore/*_sk ${TYPE}.${ORG_NAME}_sk
+	cp msp/keystore/*_sk ${TYPE}.${ORG_NAME}_sk
+fi
 
 echo "Start ${TYPE}.${ORG_NAME}..."
 fabric-ca-server start -H ${CA_PATH}
